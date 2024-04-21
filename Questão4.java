6871757da8bd40d69a79654afc2a9ebc894931bd
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-04-21 17:46:09 -0300

    Update Questão4.java

--- Questão4.java
+++ Questão4.java
@@ -1,3 +1,17 @@
+/*Faça um programa que efetue reserva de passagens aéreas de uma companhia. O programa deverá ler informações sobre os voos (número, origem e destino) e o número de lugares disponíveis para doze aviões (um vetor para cada um desses dados). Depois da leitura, o programa deverá apresentar um menu com as seguintes opções:
+consultar;
+efetuar reserva; e
+sair.
+Quando a opção escolhida for Consultar, deverá ser disponibilizado mais um menu com as seguintes opções:
+por número do voo;
+por origem; e
+por destino.
+Quando a opção escolhida for Efetuar reserva, deverá ser perguntado o número do voo em que a pessoa deseja viajar. O programa deverá dar as seguintes respostas:
+reserva confirmada — caso exista o voo e lugar disponível, dando baixa nos lugares disponíveis;
+voo lotado — caso não exista lugar disponível nesse voo;
+voo inexistente — caso o código do voo não exista.
+A opção Sair é a única que permite encerrar a execução do programa. Sendo assim, após cada operação de consulta ou reserva, o programa volta ao menu principal.
+*/
 import java.util.Scanner;
 
 public class Main {
