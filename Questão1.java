07cedfcf2a25afd71f1dcc170ecc16710aa9c1ac
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-04-21 17:45:15 -0300

    Update Questão1.java

--- Questão1.java
+++ Questão1.java
@@ -1,3 +1,5 @@
+//Faça um programa que preencha um vetor com nove números inteiros, calcule e mostre os números primos e suas respectivas posições.
+
 import java.util.Scanner;
 
 public class Main {
